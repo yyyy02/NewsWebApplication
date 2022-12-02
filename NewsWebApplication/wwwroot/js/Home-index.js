eb5a9ad4f2296d70388affcc8f2ecb082f50afc0
chenhui <1037823529@qq.com>
2022-12-02 17:51:48 +0800

    feat:Cachart

---
+++ NewsWebApplication/wwwroot/js/Home-index.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function () {
+	getTime();
+	
+});
+
+//得到当前的时间并且显示在页面上
+var getTime = function () {
+    var d = new Date();
+    var time = d.getFullYear().toString() + "年 " +
+        (d.getMonth() + 1).toString() + "月 " +
+        d.getDate().toString() + "号 ";
+    var list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+    for (var i = 0; i < list.length; i++) {
+        if (d.getDay() - 1 == i) {
+            time += list[i];
+        }
+    }
+	document.querySelector("#time").innerHTML = time
+}
+
+//轮播图
+$(function () {
+	var index = 0;
+	var timer = null;
+	var option = $('.Cachart_container>ul>li').length;
+	var imgwidth = $('.Cachart_container ul li').width();
+	var $li = $('<li><img src="./1.png" alt=""></li>')
+	$('.Cachart_container>ul').append($li);
+
+
+
+	go();
+	$(".Cachart_container").hover(function () {
+		clearInterval(timer)
+	}, function () {
+		go();
+
+	})
+	function go() {
+		$(window).resize(function () {
+			imgwidth = $('.Cachart_container ul li').width();
+		});
+		timer = setInterval(function () {
+			if (index < option) {
+				index++;
+				$(".Cachart_container ul").stop().animate({
+					left: -imgwidth * index - 20 * index + 'px'
+				})
+				console.log("success")
+			}
+			if (index == option) {
+				$(".Cachart_container ul").stop().animate({
+					left: -imgwidth * index + 'px'
+				})
+
+				index = 0;
+				$(".Cachart_container ul").animate({
+					left: -imgwidth * index + 'px'
+				}, 0)
+			}
+
+			$("ol li").eq(index).addClass('current').siblings().removeClass();
+		}, 3000)
+	}
+	$("ol li").mouseover(function () {
+		index = $(this).index();
+		$(".Cachart_container ul").animate({
+			left: -imgwidth * index - 20 * index + 'px'
+		})
+		$("ol li").eq(index).addClass('current').siblings().removeClass();
+	})
+})
--- NewsWebApplication/wwwroot/js/site.js
+++ NewsWebApplication/wwwroot/js/site.js
@@ -1,8 +1,4 @@
-﻿$(document).ready(function () {
-    getTime();
-});
-
-//改变导航栏的样式使得它适应滚动条
+﻿//改变导航栏的样式使得它适应滚动条
 $(function () {
     //监听滚动条事件，如果滚动条超过顶部导航栏的两倍，则将导航栏固定在顶部
     var header = document.querySelector('.navbar');
@@ -18,17 +14,3 @@ $(function () {
     })
 })
 
-//得到当前的时间并且显示在页面上
-var getTime = function () {
-    var d = new Date();
-    var time = d.getFullYear().toString() + "年 " +
-        (d.getMonth() + 1).toString() + "月 " +
-        d.getDate().toString() + "号 ";
-    var list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
-    for (var i = 0; i < list.length; i++) {
-        if (d.getDay()-1 == i) {
-            time += list[i];
-        }
-    }
-    document.querySelector("#time").innerHTML = time
-}

a30a52d7acaa73d68633544d031ff1e928bad443
chenhui <1037823529@qq.com>
2023-06-03 22:48:16 +0800

    feat:New css

--- NewsWebApplication/wwwroot/js/Home-admin.js
+++ NewsWebApplication/wwwroot/js/Home-admin.js
@@ -6,7 +6,7 @@
     $('#User').css("color", "rgb(69,141,255)");
     $('#Comment').css("color", "rgb(0,0,0)");
     $('#New').css("color", "rgb(0,0,0)");
-    $('#Feedback').css("color", "rgb(0,0,0)");
+    $('#Feedback').css("color", "rgb(0,0,0)");    
 }
 
 function Shownew() {
@@ -65,6 +65,24 @@ function changeUser() {
     layer.confirm('', {
         title: '修改',
         scrollbar: false,
+        content: `
+            <div class="form-container">
+                <div class="form-row">
+                    <label for="pername">用户：</label>
+                    <input type="text" id="pername" class="perin" placeholder="输入用户名" />
+                </div>
+
+                <div class="form-row">
+                    <label for="perpass">密码：</label>
+                    <input type="password" id="perpass" class="perin" placeholder="输入密码" />
+                </div>
+
+                <div class="form-row">
+                    <label for="peremail">邮箱：</label>
+                    <input type="email" id="peremail" class="perin" placeholder="输入邮箱" />
+                </div>
+            </div>
+        `,
         //area: ['800px', '350px'] //自定义文本域宽高
     }, function () {
         if ($('#pername').val() === "") {
@@ -92,15 +110,44 @@ function changeUser() {
         layer.closeAll();
         location.reload();
     });
-    $(".layui-layer-content").append("<br/>用户：<input type=\"text\" class=\"perin\" id= \"pername\" class=\"layui-input\" placeholder=\"输入用户名\"/>")
-    $(".layui-layer-content").append("<br/>密码：<input type=\"password\" class=\"perin\" id= \"perpass\" class=\"layui-input\" placeholder=\"输入密码\"/>")
-    $(".layui-layer-content").append("<br/>邮箱：<input type=\"email\" class=\"perin\" id= \"peremail\" class=\"layui-input\" placeholder=\"输入邮箱\"/>")
 }
+
+
+
 
 function changeNew() {
     layer.confirm('', {
         title: '修改',
         scrollbar: false,
+        content: `
+    <div class="form-container">
+        <div class="form-row">
+            <label for="pertitle">标题：</label>
+            <input type="text" id="pertitle" class="perin" placeholder="输入新闻标题" />
+        </div>
+
+        <div class="form-row">
+            <label for="percontent">内容：</label>
+            <textarea type="text" id="percontent" class="perin" placeholder="输入新闻内容" style="min-width:210px;height:104px"></textarea>
+        </div>
+
+        <div class="form-row">
+            <label for="pertime">时间：</label>
+            <input type="text" id="pertime" class="perin" placeholder="输入时间" />
+        </div>
+
+        <div class="form-row">
+            <label for="perColumn">栏目：</label>
+            <input type="text" id="perColumn" class="perin" placeholder="输入栏目" />
+        </div>
+
+        <div class="form-row">
+            <label for="perHeat">热度：</label>
+            <input type="text" id="perHeat" class="perin" placeholder="输入热度" />
+        </div>
+    </div>
+`,
+
         //area: ['800px', '350px'] //自定义文本域宽高
     }, function () {
         if ($('#pertitle').val() === "") {
@@ -135,11 +182,11 @@ function changeNew() {
         layer.closeAll();
         location.reload();
     });
-    $(".layui-layer-content").append("<br/>标题：<input type=\"text\" class=\"perin\" id= \"pertitle\" class=\"layui-input\" placeholder=\"输入标题\"/>")
-    $(".layui-layer-content").append("<br/>内容：<textarea type=\"password\" class=\"perin\" id= \"percontent\" class=\"layui-input\" placeholder=\"输入内容\"/>")
-    $(".layui-layer-content").append("<br/>时间：<input type=\"email\" class=\"perin\" id= \"pertime\" class=\"layui-input\" placeholder=\"输入时间\"/>")
-    $(".layui-layer-content").append("<br/>栏目：<input type=\"email\" class=\"perin\" id= \"perColumn\" class=\"layui-input\" placeholder=\"输入栏目\"/>")
-    $(".layui-layer-content").append("<br/>热度：<input type=\"email\" class=\"perin\" id= \"perHeat\" class=\"layui-input\" placeholder=\"输入热度\"/>")
+    //$(".layui-layer-content").append("<br/>标题：<input type=\"text\" class=\"perin\" id= \"pertitle\" class=\"layui-input\" placeholder=\"输入标题\"/>")
+    //$(".layui-layer-content").append("<br/>内容：<textarea type=\"password\" class=\"perin\" id= \"percontent\" class=\"layui-input\" placeholder=\"输入内容\"/>")
+    //$(".layui-layer-content").append("<br/>时间：<input type=\"email\" class=\"perin\" id= \"pertime\" class=\"layui-input\" placeholder=\"输入时间\"/>")
+    //$(".layui-layer-content").append("<br/>栏目：<input type=\"email\" class=\"perin\" id= \"perColumn\" class=\"layui-input\" placeholder=\"输入栏目\"/>")
+    //$(".layui-layer-content").append("<br/>热度：<input type=\"email\" class=\"perin\" id= \"perHeat\" class=\"layui-input\" placeholder=\"输入热度\"/>")
 }
 
 /*修改个人信息*/
@@ -162,10 +209,20 @@ function addUser() {
             return;
         }
         console.log(document.getElementById('perpass').value);
-        alert("修改成功")
+        $.ajax({
+            url: "/Login/Signup",
+            type: "POST",
+            data: { name: $('#pername').val(), password: $('#perpass').val(), email: $('#peremail').val() },
+            sucess: function (msg) {
+                alert("Signup successfully")
+            }
+        })
+        alert("添加成功")
         layer.closeAll();
     });
     $(".layui-layer-content").append("<br/>用户：<input type=\"text\" class=\"perin\" id= \"pername\" class=\"layui-input\" placeholder=\"输入用户名\"/>")
     $(".layui-layer-content").append("<br/>密码：<input type=\"password\" class=\"perin\" id= \"perpass\" class=\"layui-input\" placeholder=\"输入密码\"/>")
     $(".layui-layer-content").append("<br/>邮箱：<input type=\"email\" class=\"perin\" id= \"peremail\" class=\"layui-input\" placeholder=\"输入邮箱\"/>")
 }
+
+
